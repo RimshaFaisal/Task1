85789a96a981c12087e1df0a0cb720a45a935a21
RimshaFaisal <67114297+RimshaFaisal@users.noreply.github.com>
2020-06-18 21:24:48 +0500

    1st file

---
+++ python.py
@@ -0,0 +1,9 @@
+//conditional statements
+n1=input("enter age:")
+n1=int(n1) //t make it int otherwise input take everything as string
+if(n1>18):
+    print("drive")
+elif(n1==18):
+    print("teenager")  
+else:
+        print("cannot drive")
